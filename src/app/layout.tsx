39fa68995dc58e7958eea509536f1f6f11f488e8
Theo Browne <me@t3.gg>
2023-04-01 14:30:36 -0700

    fix topnav more

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ export const metadata = {
 
 import React from "react";
 import { ClerkProvider, UserButton } from "@clerk/nextjs/app-beta";
+import Link from "next/link";
 
 export default function RootLayout({
   children,
@@ -23,7 +24,10 @@ export default function RootLayout({
         <body>
           <div className="flex h-screen w-full grow flex-col">
             <div className="flex w-full items-center justify-between border-b border-white px-4">
-              <span className="py-4 text-2xl font-bold">MarkerThing</span>
+              <div className="py-4 text-2xl font-bold">
+                <Link href="/">MarkerThing</Link>
+              </div>
+
               <UserButton
                 afterSignOutUrl="/"
                 appearance={{
