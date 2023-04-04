831f3cf3e9eb109b27187f2e5391b0d63d248042
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-04-04 23:36:13 +0200

    mdx

---
+++ mdx-components.tsx
@@ -0,0 +1,5 @@
+import { MDXComponents } from "mdx/types";
+
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return components;
+}
--- next.config.js
+++ next.config.js
@@ -3,12 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
     runtime: "experimental-edge",
+    mdxRs: true,
   },
   images: {
     domains: ["static-cdn.jtvnw.net", "vod-secure.twitch.tv"],
   },
 };
 
+const withMdx = require("@next/mdx")()(nextConfig);
+
 const { withPlausibleProxy } = require("next-plausible");
 
-module.exports = withPlausibleProxy()(nextConfig);
+module.exports = withPlausibleProxy()(withMdx);
