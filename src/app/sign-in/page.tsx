3086b1f33041b65f046b6a8c5c549fb7addbffbb
Theo Browne <me@theo.li>
2023-04-04 13:28:30 -0700

    Revert "Fix redirects and update to latest Clerk"

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,12 +1,16 @@
 import { SignIn } from "@clerk/nextjs/app-beta";
 
-export default function Page() {
+export default function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   return (
     <div className="my-auto flex items-center justify-center">
       <SignIn
         path="/sign-in"
-        routing="path"
         signUpUrl="/sign-up"
+        redirectUrl={searchParams.redirect_url || "/"}
         appearance={{
           variables: {
             colorPrimary: "#E24A8D",
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,12 +1,16 @@
 import { SignUp } from "@clerk/nextjs/app-beta";
 
-export default function Page() {
+export default function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   return (
     <div className="my-auto flex items-center justify-center">
       <SignUp
         path="/sign-up"
-        routing="path"
         signInUrl="/sign-in"
+        redirectUrl={searchParams.redirect_url || "/"}
         appearance={{
           variables: {
             colorPrimary: "#E24A8D",
