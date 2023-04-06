c0fef30d1ad932f68638f8623fffab4068cbf4b8
Theo Browne <me@t3.gg>
2023-04-06 03:36:58 -0700

    desperate times...

--- src/app/(core)/page.tsx
+++ src/app/(core)/page.tsx
@@ -1,5 +1,7 @@
 import About from "../(components)/about.mdx";
 
+export const runtime = "nodejs";
+
 export default async function Home() {
   return (
     <div className="my-auto flex flex-col items-center justify-center">
