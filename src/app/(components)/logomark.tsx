2a801e0e6d7c0231a6ba4f70599ba389ead4d6c0
Mark R. Florkowski <mark.florkowski@gmail.com>
2023-04-06 00:03:59 -0700

    way less cursed

--- src/app/(components)/logomark.tsx
+++ src/app/(components)/logomark.tsx
@@ -4,30 +4,10 @@ import Icon from "./icon";
 import { useEffect, useState } from "react";
 
 export const LogoMark = () => {
-  /* This is a hack to get the logo to resize on mobile safari.
-   *
-   * TLDR: SVGs and flexbox don't play nice on mobile safari, so we
-   * need to manually set the SVGs size rather than using 100% width,
-   * and resizing the container.
-   */
-  const [isSmall, setIsSmall] = useState(false);
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth < 640) {
-        setIsSmall(true);
-      } else {
-        setIsSmall(false);
-      }
-    };
-    window.addEventListener("resize", handleResize);
-    handleResize();
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
   return (
     <h1 className="flex flex-row items-baseline text-2xl font-bold sm:text-4xl ">
       <Link href="/" className="flex flex-row items-baseline">
-        <Icon size={isSmall ? 24 : 32} className="-mr-2 mb-2 h-6 sm:h-8" />
+        <Icon className="-mr-2 mb-2 h-6 sm:h-8" />
         <span className="tracking-tight hover:cursor-pointer">
           {`marker`}
           <span className="text-pink-500">{`thing`}</span>
