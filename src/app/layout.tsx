a1265fd1bf3f3ce24f6b7bdf6676af4ddd8e97cf
Rickard Jarnling <rickard.jarnling@gmail.com>
2023-04-14 17:05:57 +0200

    add type for metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import "./globals.css";
+import type { Metadata } from 'next';
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 export const runtime = "edge";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "MarkerThing",
   description:
     "MarkerThing By Ping.gg - Export Twitch Markers as .csv with ease",
